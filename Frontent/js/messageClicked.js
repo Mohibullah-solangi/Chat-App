640980d6302090f4309f6601f2c113c82c8e6af0
Mohib ullah <mohib0197@gmail.com>
2024-05-16 12:18:00 +0500

    Revert "Initial commit"
    This reverts commit e9394f39cc58545846bf5089c9c5563387dd5da5.

--- Frontent/js/messageClicked.js
+++
@@ -1,9 +0,0 @@
-const socket = io("127.0.0.1:4000");
-var Users = [];
-var LoginUser = JSON.parse(window.localStorage.getItem("User"));
-
-// Message delete or update function
-export function messageClick(e){
-    // console.log(e.target.dataset['id'], "Message Click")
-    socket.emit("deleteMessage", e.target.dataset['id']);
-    }
